1083c9a742f8be4eb35aedff40a06fb314f867b7
Levi_333 <leva.nechaev2008@gmail.com>
2026-02-06 10:39:51 +0300

    Выполнена практическая и классная работы за 06.02.2026

--- Nechaev_CPP/main.cpp
+++ Nechaev_CPP/main.cpp
@@ -316,22 +316,50 @@ void Person PersonTemplateTypenames::ChangeBank()
 //    SetEducationLevel<Tid, Tlogin, Tpassword>("Высшее");
 //}
 
+// Шаблонная функция, принимающая одинаковые параметры
+template<typename T> 
+T Sum(T a, T b)
+{
+    return a + b;
+}
+
+template<typename T, typename T1>
+void swap(T& a, T1& b)
+{
+    string z = static_cast<string>(a);
+    string o = static_cast<string>(b);
+
+    string v = z;
+    z = o;
+    o = v;
+}
+
+template<typename T>
+decltype(auto) CountAverage(const T arr[], const unsigned int size)
+{
+    T result{};
+    for (unsigned int i = 0; i < size; i++)
+    {
+        result += arr[i];
+    }
+    return result / size;
+}
+
 void main() 
 {
     setlocale(LC_ALL, "Ru");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    Person<string, string, string, string, string> chelovek1{ "brawl stars", "Leon", 17, "4444 666666", 0.91, "T-BankUser333", "абоба" };
+    /*Person<string, string, string, string, string> chelovek1{ "brawl stars", "Leon", 17, "4444 666666", 0.91, "T-BankUser333", "абоба" };
     Person<int, string, string, string, int> chelovek2{ 0, "Zloy David V Mute.", 10, "0000 000000", 503.32, "Inoagent", 52 };
-    Person<double, string, string, int, int> chelovek3{ 36.6, "alex shopkeeper", 18, "4444 666666", 2000.02, 33, 64 };
+    Person<double, string, string, int, int> chelovek3{ 36.6, "alex shopkeeper", 18, "4444 666666", 2000.02, 33, 64 };*/
     
-    chelovek1.PrintInfo();
-    chelovek1.Wisdraw();
-    chelovek1.PrintInfo();
-    chelovek1.ChangeBank();
-    chelovek1.PrintInfo();
-}
+    cout << "Int: " << Sum(5, 5) << endl; // 10
+    cout << "Double: " << Sum(5.5, 5.5) << endl; // 11
+    cout << "String: " << Sum<string>("5", "5") << endl; // 55
+    cout << endl;
 
-// Практика:
-// 3. Объявить в классе функцию смены банка и секретного слова, которая просит пользователя сначала ввести секретное слово/код от ынешнего банка, а затем задать новый банк и секретное слово/код
+    int array[]{ 3, 5, 7, 9, 11, 13 };
+    cout << CountAverage(array, 6);
+}
