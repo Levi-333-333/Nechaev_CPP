9ff21baf3da7fe3ce0279b6556feab1aacab9381
Levi_333 <leva.nechaev2008@gmail.com>
2026-03-06 23:18:02 +0300

    Практика 04.03.26

--- Nechaev_CPP/main.cpp
+++ Nechaev_CPP/main.cpp
@@ -1,42 +1,83 @@
 ﻿#include <iostream>
 #include <Windows.h>
-#include <string>
 
 // Ф-я, принимающая в качестве третьего параметра указатель на функцию, которая выступает в виде лямбда-выражения
-void DoSomething(int number1, int number2, int (*operation)(int, int))
+void Calc(int number1, int number2, int (*operation)(int, int))
 {
-	std::cout << operation(number1, number2) << std::endl;
+    std::cout << "Результат операции: " << operation(number1, number2) << std::endl;
 }
 
 int main()
 {
-	setlocale(LC_ALL, "Ru");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	// Безымянная лямбда
-	[]() {std::cout << "Привет РПО" << std::endl; } ();
+    auto sum{ [](int number1, int number2) {return number1 + number2; } };
+    auto sub{ [](int number1, int number2) {return number1 - number2; } };
+    auto mul{ [](int number1, int number2) {return number1 * number2; } };
+    auto div{ [](int number1, int number2) {return number1 / number2; } };
 
-	// Именованная лямбда
-	auto hello{ []() {std::cout << "Привет РПО" << std::endl; } };
+    int userNumber1;
+    int userNumber2;
+    int userChoice;
 
-	// Лямбда с параметрами
-	auto printInfo{ [](const std::string& info) {std::cout << info << std::endl; } };
+    do
+    {
+        std::cout << "Введите цифру от 1 до 4, где:\n1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление" << std::endl;
+        std::cin >> userChoice;
+        switch (userChoice)
+        {
+        case 1:
+            std::cout << "Введите первое число: ";
+            std::cin >> userNumber1;
+            std::cout << std::endl;
 
-	// Лямбда, которая возвращает значение
-	auto sum{ [](int a, int b) {return a + b; } };
+            std::cout << "Введите второе число: ";
+            std::cin >> userNumber2;
+            std::cout << std::endl;
 
-	// Лямбда с явным возвращаемым занчением
-	std::cout << [](int x, int y) -> double {return x + y; } (5, 5) << std::endl;
+            Calc(userNumber1, userNumber2, sum);
+            break;
+        case 2:
+            std::cout << "Введите первое число: ";
+            std::cin >> userNumber1;
+            std::cout << std::endl;
 
-	// Лямбда для функции
-	auto sub{ [](int number1, int number2) {return number1 - number2; } };
+            std::cout << "Введите второе число: ";
+            std::cin >> userNumber2;
+            std::cout << std::endl;
 
-	hello();
-	printInfo("Привет РПО");
-	std::cout << sum(3, 4) << std::endl;
-	DoSomething(10, 5, sub);
+            Calc(userNumber1, userNumber2, sub);
+            break;
+        case 3:
+            std::cout << "Введите первое число: ";
+            std::cin >> userNumber1;
+            std::cout << std::endl;
 
-	system("pause");
-	return 0;
+            std::cout << "Введите второе число: ";
+            std::cin >> userNumber2;
+            std::cout << std::endl;
+
+            Calc(userNumber1, userNumber2, mul);
+            break;
+        case 4:
+            std::cout << "Введите первое число: ";
+            std::cin >> userNumber1;
+            std::cout << std::endl;
+
+            std::cout << "Введите второе число: ";
+            std::cin >> userNumber2;
+            std::cout << std::endl;
+
+            Calc(userNumber1, userNumber2, div);
+            break;
+        default:
+            std::cout << "Неверный ввод, попробуйте ещё раз!\n" << std::endl;
+            break;
+        }
+    } while (!(1 <= userChoice && userChoice <= 4));
+
+    system("pause");
+    return 0;
 }
