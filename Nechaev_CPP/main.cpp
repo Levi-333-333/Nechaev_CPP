5928222bdce7dc7f5e24fae12cfa2f9af833b04d
Levi_333 <leva.nechaev2008@gmail.com>
2026-03-12 01:42:03 +0300

    ДЗ 3.03.2026

--- Nechaev_CPP/main.cpp
+++ Nechaev_CPP/main.cpp
@@ -1,11 +1,83 @@
 ﻿#include <iostream>
 
+class Light
+{
+public:
+    void TurnOn()
+    {
+        std::cout << "Лампочка включена" << std::endl;
+    }
+    void TurnOff()
+    {
+        std::cout << "Лампочка выключна" << std::endl;
+    }
+private:
+    std::string model;
+};
+
+class Thermostat
+{
+public:
+    void TurnOn()
+    {
+        std::cout << "Термостат включен" << std::endl;
+    }
+    void TurnOff()
+    {
+        std::cout << "Термостат выключно" << std::endl;
+    }
+private:
+    std::string model;
+};
+
+class Radio
+{
+public:
+    void TurnOn()
+    {
+        std::cout << "Радио включен" << std::endl;
+    }
+    void TurnOff()
+    {
+        std::cout << "Радио выключно" << std::endl;
+    }
+private:
+    std::string model;
+};
+
+class SmartHome
+{
+public:
+    SmartHome(Light* light, Thermostat* thermostat, Radio* radio)
+    {
+        this->light = light;
+        this->thermostat = thermostat;
+        this->radio = radio;
+    }
+    void TurnOn()
+    {
+        light->TurnOn();
+        thermostat->TurnOn();
+        radio->TurnOn();
+    }
+    void TurnOff()
+    {
+        light->TurnOff();
+        thermostat->TurnOff();
+        radio->TurnOff();
+    }
+private:
+    Light* light;
+    Thermostat* thermostat;
+    Radio* radio;
+};
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
 
-    int a{ 4.5 }; // В данном случае программа не запуститься. Фигурные скобки не позволяют преобразовать из одного типа данных в другой. Поэтому она более безопасна
-    int b = 4.5; // В данном случае компилятор выдаст предупреждение о возможной потере данных
-    int c(4.5); // В данном случае компилятор выдаст предупреждение о возможной потере данных
+    SmartHome sillyHome(new Light, new Thermostat, new Radio);
+
+    sillyHome.TurnOn();
+    sillyHome.TurnOff();
 }
