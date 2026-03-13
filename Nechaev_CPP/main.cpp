e3d71a26021822b821b6d4483ea0b970f91f8388
Levi_333 <leva.nechaev2008@gmail.com>
2026-03-13 12:51:28 +0300

    HW 05.03.2026

--- Nechaev_CPP/main.cpp
+++ Nechaev_CPP/main.cpp
@@ -1,154 +1,198 @@
 ﻿#include <string>
+#include <Windows.h>
 #include <iostream>
-#include <list>
 
-class IObserver // интерфейс для наблюдателя
+class Context;
+
+// Включает в себя состояния программы и контекст, который может быть использован, как способ передать состояние другому объекту.
+class State
 {
 public:
-    virtual ~IObserver() {};
-    virtual void Update(const std::string& message) = 0; // Функция, обновляющая поступающую информацию наблюдаемому
+    ~State() {}
+    // Публичный метод, позволяющий переключиться на конкретное состояние
+    void SetContext(Context* context)
+    {
+        this->context = context;
+    }
+    virtual void DoSomething1() = 0;
+    virtual void DoSomething2() = 0;
+protected:
+    Context* context;
 };
 
-class ISubject // интерфейс для наблюдамого
+// Определяет интерфейс, позволяющий клиентам обратиться к конкретному состоянию
+class Context
 {
 public:
-    virtual ~ISubject() {};
-    virtual void Subscribe(IObserver* observer) = 0; // Функция, подписывающая на наблюдателя
-    virtual void UnSubscribe(IObserver* observer) = 0; // Функция, отписывающая от наблюдателя
-    virtual void Notify() = 0; // Уведомить наблюдаемого
-};
-
-class Subject : public ISubject
-{
-public:
-    ~Subject()
+    Context(State* state, std::string name)
     {
-        std::cout << "Наблюдаемый был очищен в памяти" << std::endl;
+        this->TransitionToState(state);
+        this->name = name;
     }
-
-    // Методы управления подпиской
-    void Subscribe(IObserver* observer) override
+    // Контекст позволяет изменять состояние объекта с помощью этой функции
+    void TransitionToState(State* state)
     {
-        observerList.push_back(observer);
-    }
-
-    void UnSubscribe(IObserver* observer) override
-    {
-        observerList.remove(observer);
-    }
-
-    void Notify() override
-    {
-        // Для того, чтобы определить свою единицу перечисления для такого типа данных, как список, создается итератор, а внего задается начальный объект списка
-        std::list<IObserver*>::iterator iterator = observerList.begin();
-        std::cout << "За объектом наблюдает " << observerList.size() << " наблюдателей" << std::endl;
-        // Пока значение итератора не примет вид последнего объекта из списка
-        while (iterator != observerList.end())
+        std::cout << "Происходит смена контекста на другое состояние " << typeid(*state).name() << std::endl;
+        // Если присутствует состояние у контекста, очищаем память указателя на состояние и задаем новое
+        if (this->state != nullptr)
         {
-            // Вызываем функцию Update у текущего перечисляемого наблюдателя
-            (*iterator)->Update(message);
-            ++iterator; // У объекта итератора перегружен оператор ++
+            delete this->state;
         }
+        this->state = state;
+        this->state->SetContext(this);
     }
-
-    // Функция, при вызове которой задается новое сообщение (новое состояние) для наблюдаемого и происходит автоматическое уведомление наблюдателя через функцию Notify()
-    void CreateMessage(std::string message)
+    void Request1()
     {
-        this->message = message;
-        Notify();
+        this->state->DoSomething1();
     }
-
-    std::list<IObserver*> GetObserverList()
+    void Request2()
     {
-        return observerList;
+        this->state->DoSomething2();
+    }
+    std::string GetName()
+    {
+        return name;
+    }
+    ~Context()
+    {
+        delete state;
     }
 private:
-    std::list<IObserver*> observerList;
-    std::string message;
+    State* state;
+    std::string name;
 };
 
-class Observer : public IObserver
+class SpellState;
+
+class AttackState : public State
 {
 public:
-    // Конструктор
-    Observer(Subject& _subject, std::string _name, int _answer) : subject(_subject), name(_name), answer(_answer)
+    void DoSomething2() override;
+
+    void DoSomething1() override
     {
-        // Наблюдаемый в классе наблюдателя подписывается на второго сразу же в конструкторе
-        this->subject.Subscribe(this);
-        couterObservers++;
-        std::cout << "Наблюдатель " << couterObservers << " проинициализирован" << std::endl;
+        std::cout << context->GetName() << " атакует!" << std::endl;
     }
-    // Деструктор
-    ~Observer()
+};
+
+class DodgeState : public State
+{
+    // Переопределяемые функции, позволяющие выполнить специфичную логику, характерную для состояния. Например, логика нанесения урона врагу при состоянии атаки или повышение ловкости при состоянии уворота
+public:
+    void DoSomething1() override
     {
-        couterObservers--;
-        std::cout << "Наблюдатель " << name << " был очищен" << std::endl;
-    }
-    // Переопределенная Update
-    void Update(const std::string& message) override
-    {
-        this->message = message;
-        // Наблюдатель должен реагировать на поступаемое изменение
-        std::cout << "У наблюдателя " << name << " появилось новое сообщение от наблюдаемого: " << this->message << std::endl;
-    }
-    // Вспомогательная функция по управлению количеством наблюдаемых
-    void UnsubscribeSubject()
-    {
-        subject.UnSubscribe(this);
-        std::cout << "Наблюдаемый " << name << " был отписан от наблюдателя!" << std::endl;
+        std::cout << context->GetName() << " уворачивается!" << std::endl;
     }
 
-    int GetAnswer()
-    {
-        return answer;
-    }
-private:
-    Subject& subject;
-    std::string message;
-    static inline unsigned int couterObservers;
-    std::string name;
-    int answer;
+    void DoSomething2() override;
 };
+
+class SpellState : public State
+{
+public:
+    void DoSomething1() override
+    {
+        std::cout << context->GetName() << " использует Игнис!" << std::endl;
+    }
+
+    void DoSomething2() override
+    {
+        std::cout << "SpellState обрабатывает запрос" << std::endl;
+        std::cout << "SpellState пытается изменить состояние контекста" << std::endl;
+
+        this->context->TransitionToState(new AttackState);
+    }
+};
+
+void AttackState::DoSomething2()
+{
+    std::cout << "AttackState обрабатывает запрос" << std::endl;
+    std::cout << "AttackState пытается изменить состояние контекста" << std::endl;
+
+    this->context->TransitionToState(new DodgeState);
+}
+
+void DodgeState::DoSomething2()
+{
+    std::cout << "DodgeState обрабатывает запрос" << std::endl;
+    std::cout << "DodgeState пытается изменить состояние контекста" << std::endl;
+
+    this->context->TransitionToState(new SpellState);
+}
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    Subject* subjectLeon = new Subject;
-    Observer* observerGreka = new Observer(*subjectLeon, "Greka", 3);
-    Observer* observerRak = new Observer(*subjectLeon, "Rak", 2);
-    Observer* observerReka = new Observer(*subjectLeon, "Reka", -2);
+    Context* player;
+    Context* goblin_boblin;
+    int userChoise;
 
-    int userAnswer = 0;
-
-    subjectLeon->CreateMessage("РЕШИТЬ УРАВНЕНИЕ: (x + 5) - 3 = 0");
-    std::cin >> userAnswer;
-    if (!(userAnswer == observerReka->GetAnswer())) observerReka->UnsubscribeSubject();
-    userAnswer = 0;
-
-    subjectLeon->CreateMessage("РЕШИТЬ УРАВНЕНИЕ: x^2 - 4x + 4 = 0");
-    std::cin >> userAnswer;
-    if (!(userAnswer == observerRak->GetAnswer())) observerRak->UnsubscribeSubject();
-    userAnswer = 0;
-
-    subjectLeon->CreateMessage("РЕШИТЬ УРАВНЕНИЕ: x^2 - 6x + 9 = 0");
-    std::cin >> userAnswer;
-    if (!(userAnswer == observerGreka->GetAnswer())) observerGreka->UnsubscribeSubject();
-    userAnswer = 0;
-
-    if (subjectLeon->GetObserverList().empty())
+    do
     {
-        std::cout << "Ты слишком тупой для этого дерьма. Ты под наблюдением." << std::endl;
-        Observer* observerTwichModer = new Observer(*subjectLeon, "Twich moderator", NULL);
-        observerTwichModer->Update("Теперь я слежу за тобой.");
+        std::cout << "Выберите действие\n1. Атака\n2. Уворот\n3. Заклинание\n";
+        std::cin >> userChoise;
+        std::cout << std::endl;
 
-        delete observerTwichModer;
-    }
+        // Гоблин боблин всегда круче
+        switch (userChoise)
+        {
+        case 1:
+            player = new Context(new AttackState, "player");
+            goblin_boblin = new Context(new DodgeState, "goblin-boblin");
 
-    delete observerGreka;
-    delete observerRak;
-    delete observerReka;
-    delete subjectLeon;
+            for (int i = 0; i < 3; i++)
+            {
+                player->Request1();
+                player->Request2();
 
+                goblin_boblin->Request1();
+                goblin_boblin->Request2();
+            }   
+
+            delete player;
+            delete goblin_boblin;
+            break;
+        case 2:
+            player = new Context(new DodgeState, "player");
+            goblin_boblin = new Context(new SpellState, "goblin-boblin");
+
+            for (int i = 0; i < 3; i++)
+            {
+                player->Request1();
+                player->Request2();
+
+                goblin_boblin->Request1();
+                goblin_boblin->Request2();
+            }
+
+            delete player;
+            delete goblin_boblin;
+            break;
+        case 3:
+            player = new Context(new SpellState, "player");
+            goblin_boblin = new Context(new AttackState, "goblin-boblin");
+
+            for (int i = 0; i < 3; i++)
+            {
+                player->Request1();
+                player->Request2();
+
+                goblin_boblin->Request1();
+                goblin_boblin->Request2();
+            }
+
+            delete player;
+            delete goblin_boblin;
+            break;
+        default:
+            std::cout << "Нормально пиши есть же" << std::endl;
+            break;
+        }
+    } while (!(1 <= userChoise <= 3));
+
+    system("pause");
     return 0;
 }
